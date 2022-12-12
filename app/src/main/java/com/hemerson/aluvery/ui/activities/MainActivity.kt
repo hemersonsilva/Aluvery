97bae34d7d9159b9c843f235b7c4309a3918ec88
hemersonsilva <hemerson.silva@apektelecom.com>
2022-12-11 21:37:00 -0300

    Add FAB

--- app/src/main/java/com/hemerson/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/hemerson/aluvery/ui/activities/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.hemerson.aluvery.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.hemerson.aluvery.sampledata.sampleSections
 import com.hemerson.aluvery.ui.screens.HomeScreen
 import com.hemerson.aluvery.ui.theme.AluveryTheme
@@ -13,18 +20,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            App(onFabClick = {
+                    startActivity(Intent(this, ProductFormActivity::class.java))
+                }
+            )
         }
     }
 }
 
 @Composable
-fun App() {
+fun App(onFabClick: () -> Unit = {}) {
     AluveryTheme {
         Surface {
-            HomeScreen(
-                sampleSections
-            )
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = onFabClick) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }) {
+                HomeScreen(
+                    sampleSections
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
 }
