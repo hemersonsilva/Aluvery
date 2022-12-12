b29f3a23c3a00d9418c9719761239d543dbe40fd
hemersonsilva <hemerson.silva@apektelecom.com>
2022-12-11 21:25:33 -0300

    Melhorias da tela de formulário do produto

--- app/src/main/java/com/hemerson/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/hemerson/aluvery/ui/activities/ProductFormActivity.kt
@@ -5,10 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -19,10 +24,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.hemerson.aluvery.R
+import com.hemerson.aluvery.model.Product
 import com.hemerson.aluvery.ui.theme.AluveryTheme
+import java.math.BigDecimal
+import java.text.DecimalFormat
 
 class ProductFormActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,45 +58,114 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
 
         var url by remember { mutableStateOf("") }
         var name by remember { mutableStateOf("") }
         var price by remember { mutableStateOf("") }
         var description by remember { mutableStateOf("") }
+        val formatter = remember { DecimalFormat("#.##") }
 
+        Spacer(modifier = Modifier)
         Text(
             text = "Criando um Produto", Modifier.fillMaxWidth(), fontSize = 28.sp
         )
 
-        TextField(value = url, onValueChange = {
-            url = it
-        }, Modifier.fillMaxWidth(), label = {
-            Text(text = "Url da imagem")
-        })
-        TextField(value = name, onValueChange = {
-            name = it
-        }, Modifier.fillMaxWidth(), label = {
-            Text(text = "Nome")
-        })
-        TextField(value = price, onValueChange = {
-            price = it
-        }, Modifier.fillMaxWidth(), label = {
-            Text(text = "Preço")
-        })
-        TextField(value = description, onValueChange = {
-            description = it
-        }, Modifier.fillMaxWidth().heightIn(min = 100.dp), label = {
-            Text(text = "Descrição")
-        })
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
+        }
 
-        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
+        TextField(
+            value = url,
+            onValueChange = { url = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Url da imagem")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next
+            )
+        )
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Nome")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next,
+                capitalization = KeyboardCapitalization.Words
+            )
+        )
+        TextField(
+            value = price,
+            onValueChange = {
+                try {
+                    price = formatter.format(BigDecimal(it))
+                } catch (e: java.lang.IllegalArgumentException) {
+                    if (it.isBlank()) {
+                        price = it
+                    }
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Preço")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next
+            )
+        )
+
+        TextField(
+            value = description,
+            onValueChange = { description = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp), label = {
+                Text(text = "Descrição")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                val convertedPrice = try {
+                    BigDecimal(price)
+                } catch (e: NumberFormatException) {
+                    BigDecimal.ZERO
+                }
+                val product = Product(
+                    name = name,
+                    image = url,
+                    price = convertedPrice,
+                    description = description
+                )
+            },
+        ) {
             Text(text = "Salvar")
         }
+        Spacer(modifier = Modifier)
     }
-
-
 }
 
 @Preview(showBackground = true)
